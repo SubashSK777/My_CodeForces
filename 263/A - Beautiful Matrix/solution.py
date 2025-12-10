9c158ee3127e534b1fd96a9331bb7f2e7340d033
Subash Kumar <subashsk11831@gmail.com>
2025-12-10 11:12:29 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.py
@@ -0,0 +1,1 @@
+arr = [list(map(int, input().split())) for i in range(5)]  for i in range(5):    for j in range(5):        if arr[i][j] == 1:            res = ((abs(2 - i)) + (abs(2 - j)))print(res)
